76d807798adad0f38ea68cf3e936e84a5dbaa625
Kresimir Bratkovic <kbratkovic@gmail.com>
2022-07-23 14:20:31 +0200

    Model changes

---
+++ VsiteZavrsniRad/Models/SparePart_WorkOrder.cs
@@ -0,0 +1,12 @@
+﻿namespace VsiteZavrsniRad.Models;
+
+public class SparePart_WorkOrder
+{
+    public int Id { get; set; }
+
+    public int SparePartId { get; set; }
+    public SparePartModel? SparePart { get; set; }
+
+    public int WorkOrderId { get; set; }
+    public WorkOrderModel? WorkOrder { get; set; }
+}
--- VsiteZavrsniRad/Models/WorkOrderModel.cs
+++ VsiteZavrsniRad/Models/WorkOrderModel.cs
@@ -11,11 +11,12 @@ public class WorkOrderModel
 {
     public int Id { get; set; }
 
-    public int WorkOrderNumber { get; set; }
+    public string WorkOrderNumber { get; set; } = string.Empty;
 
     public DateTime Date { get; set; }
 
-    public ClientModel Client { get; set; } = new ClientModel();
+    [MaxLength(100)]
+    public string ServiceItem { get; set; } = string.Empty;
 
     [Required(ErrorMessage = "Status naloga je obavezan")]
     [MaxLength(50)]
@@ -24,5 +25,11 @@ public class WorkOrderModel
     [MaxLength(500)]
     public string WorkDescription { get; set; } = string.Empty;
 
-    public List<SparePartModel> SpareParts { get; set; } = new List<SparePartModel>();
+
+    // Navigation Properties
+    public int ClientId { get; set; }
+    public ClientModel Client { get; set; } = new ClientModel();
+    public List<SparePart_WorkOrder> SparePart_WorkOrders { get; set; } = new List<SparePart_WorkOrder>();
+
+
 }
